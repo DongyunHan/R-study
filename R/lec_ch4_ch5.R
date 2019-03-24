c06a2d70e4b86dd50d675e9e4aa9beb691765c36
handy <handy113@unist.ac.kr>
2019-03-24 21:59:28 +0900

    init

---
+++ R/lec_ch4_ch5.R
@@ -0,0 +1,17 @@
+#R lec4
+# str() -> show structure of table
+
+# typeof() -> show type of variable
+# mode()
+# class() 
+
+#R lec5
+# sum()
+# mean()
+# mean() = sum() / length()
+# max()
+# min()
+
+# y = c(1:100)
+## y <- [1,2,3,4 ... 98,99,100]
+## R에서는 list의 index가 0 부터 시작하지 않고 1부터 시작한다.
